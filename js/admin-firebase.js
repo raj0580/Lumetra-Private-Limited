41386dc81ad7436d30ac521a229849228fe9a7df
raj0580 <duckdikcow@gmail.com>
2025-07-19 16:01:29 +0530

    Update admin-firebase.js

--- js/admin-firebase.js
+++ js/admin-firebase.js
@@ -1,5 +1,5 @@
 /* ==========================================================================
-   Central Firebase Configuration for the Lumetra Admin Panel
+   Central Firebase & API Key Configuration for the Lumetra Admin Panel
    ========================================================================== */
 
 // --- 1. CONFIGURATION ---
@@ -14,23 +14,19 @@ const firebaseConfig = {
   appId: "1:633062556570:web:d9cdc60726444ba7fd3023"
 };
 
+const imgbbApiKey = '5090ec8c335078581b53f917f9657083';
+
 
 // --- 2. INITIALIZATION ---
-// Initialize Firebase and export the services for other scripts to use.
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-// We no longer use storage directly in the new ImgBB version, but it's good practice to keep it here if needed later.
-// const storage = firebase.storage(); 
 
 
 // --- 3. SECURITY GUARD ---
-// This function checks if a user is logged in. If not, it kicks them out.
-// We exclude the login page itself from this check.
 if (!window.location.pathname.endsWith('admin.html')) {
     auth.onAuthStateChanged(user => {
         if (!user) {
-            console.log("No user logged in. Redirecting to login page.");
             window.location.href = 'admin.html';
         }
     });
